46806e83b877fbc621b4796f200e3b838c5264d1
Ben Clemens <ben.clemens@gmail.com>
2015-01-13 20:58:14 -0800

    basic calls to diy API

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,14 @@
 var express = require('express');
 var router = express.Router();
+var request = require('request');
 
 /* GET home page. */
 router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+  request.get({url:'https://api.diy.org/explore/featured?limit=5&offset=0', json:true}, function (error, response, body) {
+	  if (!error && response.statusCode == 200) {
+	  	res.render('index', { title: 'on camp', data: body });
+	  }
+  })
 });
 
 module.exports = router;
