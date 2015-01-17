a0fb610e0349afa6ce17442470c29f8fb78242fe
Ben Clemens <ben.clemens@gmail.com>
2015-01-17 00:30:32 -0800

    static project detail page

--- app.js
+++ app.js
@@ -23,6 +23,9 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', routes);
 app.use('/maker', maker);
+app.get('/project', function(req, res) {
+    res.render('project', {title: 'Project'});
+});
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
